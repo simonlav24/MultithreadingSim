44118bae76df0d7f77d61d316e6bba8a45bee5d5
simonLab <simelav24@gmail.com>
2021-06-15 20:05:32 +0300

    python code blocked

--- hw4sketch.py
+++ hw4sketch.py
@@ -22,6 +22,9 @@ CMD_HALT  = 7
 
 instDict = {CMD_NOP:"nop", CMD_ADD:"add", CMD_SUB:"sub", CMD_ADDI:"addi", CMD_SUBI:"subi", CMD_LOAD:"load", CMD_STORE:"store", CMD_HALT:"halt"}
 
+# TODO
+# what if overhead > 1
+
 class Thread:
 	"""thread class for parsing purposes"""
 	def __init__(self, num, offset):
@@ -35,11 +38,23 @@ class Thread:
 		self.context = None
 		
 		self.waitCycle = 0
+	
+	def updateWaitCycles(self, cycles):
+		if self.waitCycle > 0:
+			self.waitCycle -= 1
+			if self.waitCycle == 0:
+				self.ready = True
+	
 	def __str__(self):
 		string = "T" + str(self.num) + " " + hex(self.offset) + ": " + str(self.program)
 		return string
 	def __repr__(self):
 		return str(self)
+	def printState(self):
+		print("[", self.num, ":", self.waitCycle, "]")
+	def printRegs(self):
+		print("Register file thread id", self.num, ":")
+		print(self.context)
 
 class Instruction:
 	"""Instruction class as seen in core_api.h"""
@@ -54,12 +69,19 @@ class tcontext:
 	""" registers array per thread as seen in core_api.h"""
 	def __init__(self):
 		self.regs = [0] * REGS_COUNT
+	def __str__(self):
+		string = '\t'
+		for i, val in enumerate(self.regs):
+			string += "R" + str(i) + " = " + hex(val) + "\t"
+		return string
+	def __repr__(self):
+		return str(self)
 
 def SIM_MemInstRead(line, tid):
 	string = threadsPrograms[tid].program[line]
 	dst = Instruction()
 	string = string.split()
-	print(string)
+	# print(string)
 	
 	if string[0] == 'LOAD':
 		dst.opcode = CMD_LOAD
@@ -71,7 +93,6 @@ def SIM_MemInstRead(line, tid):
 		else:
 			dst.src2_index_imm = int(string[3], 16)
 			dst.isSrc2Imm = True
-	
 	elif string[0] == 'STORE':
 		dst.opcode = CMD_STORE
 		dst.dst_index = int(string[1][1:-1])
@@ -82,42 +103,35 @@ def SIM_MemInstRead(line, tid):
 		else:
 			dst.src2_index_imm = int(string[3], 16)
 			dst.isSrc2Imm = True
-			
 	elif string[0] == 'ADD':
 		dst.opcode = CMD_ADD
 		dst.dst_index = int(string[1][1:-1])
 		dst.src1_index = int(string[2][1:-1])
 		dst.src2_index_imm = int(string[3][1:])
 		dst.isSrc2Imm = False
-		
 	elif string[0] == 'ADDI':
 		dst.opcode = CMD_ADDI
 		dst.dst_index = int(string[1][1:-1])
 		dst.src1_index = int(string[2][1:-1])
 		dst.src2_index_imm = int(string[3], 16)
 		dst.isSrc2Imm = True
-		
 	elif string[0] == 'SUB':
 		dst.opcode = CMD_SUB
 		dst.dst_index = int(string[1][1:-1])
 		dst.src1_index = int(string[2][1:-1])
 		dst.src2_index_imm = int(string[3][1:-1])
 		dst.isSrc2Imm = False
-		
 	elif string[0] == 'SUBI':
 		dst.opcode = CMD_SUBI
 		dst.dst_index = int(string[1][1:-1])
 		dst.src1_index = int(string[2][1:-1])
 		dst.src2_index_imm = int(string[3], 16)
 		dst.isSrc2Imm = True
-	
 	elif string[0] == 'HALT':
 		dst.opcode = CMD_HALT
 		# $0 - ???
-		
 	elif string[0] == 'NOP':
 		dst.opcode = CMD_NOP
-	
 	return dst
 
 def loadFromMem(address):
@@ -145,7 +159,6 @@ def executeInst(inst, threadId):
 		param1 = threadsPrograms[threadId].context.regs[inst.src1_index]
 		param2 = inst.src2_index_imm
 		threadsPrograms[threadId].context.regs[inst.dst_index] = param1 - param2
-		
 	elif inst.opcode == CMD_LOAD:
 		base = threadsPrograms[threadId].context.regs[inst.src1_index]
 		offset = inst.src2_index_imm
@@ -156,38 +169,109 @@ def executeInst(inst, threadId):
 		offset = inst.src2_index_imm
 		value = threadsPrograms[threadId].context.regs[inst.src1_index]
 		storeToMem(base + offset, value)
-		
+
+S_COMMAND = 0
+S_EVENT = 1
+S_OVERHEAD = 2
+S_IDLE = 3
+
 def CORE_BlockedMT():
-	
 	currentThreadId = 0
 	totalCycles = 0
+	overheadingCounter = 0
+	state = S_COMMAND
+	totalInst = 0
 	
 	while True:
-		print("threadID:", currentThreadId)
-		# fetch instruction:
-		currentPc = threadsPrograms[currentThreadId].pc
-		currentInst = SIM_MemInstRead(currentPc, currentThreadId)
-		print("\tinstruction:", instDict[currentInst.opcode])
-		threadsPrograms[currentThreadId].pc += 1
+		# if totalCycles > 30:
+			# break
 		
-		# execute instruction:
-		executeInst(currentInst, currentThreadId)
+		# check if any active
+		if not any([i.active for i in threadsPrograms]):
+			break
+			
+		# check if current is ready
+		if not threadsPrograms[currentThreadId].ready:
+			state = S_IDLE
 		
-		if currentInst.opcode in [CMD_STORE, CMD_LOAD, CMD_HALT]:
-			# context switch
-			print("---context switch---")
+		if state == S_COMMAND:
+			# fetch instruction:
+			currentPc = threadsPrograms[currentThreadId].pc
+			currentInst = SIM_MemInstRead(currentPc, currentThreadId)
+			executeInst(currentInst, currentThreadId)
+			threadsPrograms[currentThreadId].pc += 1
+			print(totalCycles, ":", currentThreadId, ":", instDict[currentInst.opcode])
+			
+			if currentInst.opcode in [CMD_STORE, CMD_LOAD, CMD_HALT]:
+				state = S_EVENT
+			
+			totalCycles += 1
+			totalInst += 1
+			[i.updateWaitCycles(OVERHEAD) for i in threadsPrograms]
+		
+		elif state == S_EVENT:
 			threadsPrograms[currentThreadId].ready = False
+			# thread wait
+			if currentInst.opcode == CMD_STORE:
+				threadsPrograms[currentThreadId].waitCycle = STORE_LATENCY
+			if currentInst.opcode == CMD_LOAD:
+				threadsPrograms[currentThreadId].waitCycle = LOAD_LATENCY
+			if currentInst.opcode == CMD_HALT:
+				threadsPrograms[currentThreadId].active = False
+				
+			# try to context switch
 			readyThreads = [i.ready for i in threadsPrograms]
 			if any(readyThreads):
 				for i in range(THREAD_NUM):
 					candidateThread = (currentThreadId + i + 1) % THREAD_NUM
 					if threadsPrograms[candidateThread].ready:
 						currentThreadId = candidateThread
-						totalCycles += OVERHEAD
+						state = S_OVERHEAD
+						overheadingCounter = OVERHEAD
+						# print("--- context switch on", totalCycles, "to:", currentThreadId, "---")
 						break
-			else:
-				print("---no ready threads---")
-				totalCycles += 1
+			# else:
+				# print("---no ready threads, continuing with current---")
+
+		elif state == S_OVERHEAD:
+			print(totalCycles, ":", " ", ":", "overhead")
+			totalCycles += 1
+			[i.updateWaitCycles(OVERHEAD) for i in threadsPrograms]
+			overheadingCounter -= 1
+			if overheadingCounter == 0:
+				state = S_COMMAND
+		
+		elif state == S_IDLE:
+			print(totalCycles, ":", " ", ":", "idle")
+			totalCycles += 1
+			[i.updateWaitCycles(OVERHEAD) for i in threadsPrograms]
+			
+			if threadsPrograms[currentThreadId].ready:
+				state = S_COMMAND
+				continue
+			
+			readyThreads = [i.ready for i in threadsPrograms]
+			if any(readyThreads):
+				for i in range(THREAD_NUM):
+					candidateThread = (currentThreadId + i + 1) % THREAD_NUM
+					if threadsPrograms[candidateThread].ready:
+						if currentThreadId == candidateThread:
+							state = S_COMMAND
+						else:
+							currentThreadId = candidateThread
+						state = S_OVERHEAD
+						overheadingCounter = OVERHEAD
+						# totalCycles += OVERHEAD; print("cycle after overhead:", totalCycles)
+						# print("--- context switch on", totalCycles, "to:", currentThreadId, "---")
+						break
+			# else:
+				# print("---no ready threads, continuing with current---")
+			
+	
+	print("---- Blocked MT Simulation ----\n")
+	for i in range(THREAD_NUM):
+		threadsPrograms[i].printRegs()
+	print("Blocked MT CPI for this program", totalCycles / totalInst)
 			
 ################################################################################## MAIN
 
@@ -236,7 +320,6 @@ for line in file:
 		threadsPrograms[thread].program.append(line)
 	
 	if segment == "data":
-		print(line)
 		if '0x' in line:
 			dataArr[dataIndex] = int(line, 16)
 			dataIndex += 1
@@ -245,10 +328,11 @@ for line in file:
 			dataIndex += 1
 
 # print parsed info
-print(LOAD_LATENCY, STORE_LATENCY, OVERHEAD, THREAD_NUM)
-print(threadsPrograms)
-print(hex(dataOffset))
-print(dataArr)
+if False:
+	print(LOAD_LATENCY, STORE_LATENCY, OVERHEAD, THREAD_NUM)
+	print(threadsPrograms)
+	print(hex(dataOffset))
+	print(dataArr)
 
 CORE_BlockedMT()
 
